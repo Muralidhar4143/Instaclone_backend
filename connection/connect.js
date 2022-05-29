5947e24c960c4d9cb3b6cf79f9e7d2d67f47b189
Muralidhar4143 <ragitimuralidhar@gmail.com>
2022-05-29 12:33:25 +0530

    make it better

--- connection/connect.js
+++ connection/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function main() {
   await mongoose.connect(
-    "mongodb+srv://Murali:murali@cluster0.8cbbp.mongodb.net/instaclone?retryWrites=true&w=majority", () => {
+    "mongodb+srv://Murali:murali@cluster0.8cbbp.mongodb.net/instaclone-4143?retryWrites=true&w=majority", () => {
     console.log("mongoose connected")
     });
 }
